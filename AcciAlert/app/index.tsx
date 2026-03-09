dd5a99ce8070cdf61926c641eba50e1810c6d098
Chess-nut <chesterlegarde33@gmail.com>
2026-03-09 22:11:18 +0800

    getting started screen

---
+++ AcciAlert/app/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import GettingStartedScreen from './GettingStartedScreen'; // import the new file
+
+export default function App() {
+  return <GettingStartedScreen navigation={null} />; // pass null for now
+}
